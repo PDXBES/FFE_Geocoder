7575ff940ad4a7ecf473ca2648ac416ec41dd923
Brent Freeman <freembre@oregonstate.edu>
2019-10-28 16:43:33 -0700

    functions will now push surveys through three stage geocoding process with using regular expressions on the final pass with the address locator. Final feature class will also note how the addess was geocoded and if there are any unmatched

--- FFE_Creator.py
+++ FFE_Creator.py
@@ -4,34 +4,73 @@ import layer_creator
 
 ### this section is for user defined variables
 
-input_excel = r"C:\temp\ffe_scratch\surveys\2019-03-29 E10216 Stark HSS-17 MERGE Original Request FFE JMF.xlsx"
+input_excel = r"C:\temp\ffe_scratch\surveys\Surveyed_E10345_RoseCity.xls"
 excel_sheet = "Sheet1"
+
+
+
 output_gdb = r"C:\temp\ffe_scratch\FFE_working.gdb"
+
+
+ffe_main = output_gdb + "/FFE_points_main"
+ffe_taxlots = output_gdb + "/FFE_points_taxlots"
+
 output_mxd_path = r"C:\temp\ffe_scratch\output"
 ffe_points = output_gdb + "/FFE_points"
+ffe_template_path = r"C:\temp\ffe_scratch\FFE_working.gdb\ffe_template"
 
-#using output_gdb now, will transition to in memory
+#TODO: transistion to in-memory workspace for all temporary files
 
 if not arcpy.Exists(output_gdb):
     arcpy.CreateFileGDB_management(r"C:\temp\ffe_scratch", "FFE_working.gdb")
 
+feature_classes_to_append = []
+
+
+# Create a table from the input ffe excel spread sheet
+new_table = layer_creator.create_ffe_points_layer(input_excel, excel_sheet, output_gdb)
+
+# Create an empty feature class from a template
+feature_class_name = layer_creator.create_point_feature_class_with_template("FFE_points_main", output_gdb, ffe_template_path)
+additional_fc = layer_creator.create_point_feature_class_with_template("FFE_points_taxlots", output_gdb, ffe_template_path)
+#geo_locater_fc = layer_creator.create_point_feature_class_with_template("FFE_points_locator", output_gdb, ffe_template_path)
+
+# Geocode the ffe points using a first pass from master address points and a second pass from the address locator
+unmatched_address_list = layer_creator.geocode_ffe_points_with_master_address_points(new_table, feature_class_name)
+
+
+if len(unmatched_address_list) > 0:
+
+    unmatched_address_table = layer_creator.create_table_from_list(unmatched_address_list, output_gdb, "unmatched_ffe")
+
+
+    #  ffe points using the taxlot addresses
+
+    unmatched_address_list_2 = layer_creator.geocode_ffe_points_with_taxlots(unmatched_address_table, additional_fc)
+    feature_classes_to_append.append(ffe_taxlots)
+    if len(unmatched_address_list_2) > 0:
+        filtered_list = layer_creator.remove_address_ranges_from_list_of_addresses(unmatched_address_list_2)
+        unmatched_address_table_2 = layer_creator.create_table_from_list(filtered_list, output_gdb, "unmatched_ffe_2")
+
+
+        ###Go over the uncoded points using the adddress locator
+        geo_locater_fc_path = output_gdb + "/geocoded_ffe"
+
+        layer_creator.add_text_field_to_feature_class(unmatched_address_table_2, "conditioned_address", 100)
+
+
+        layer_creator.update_field_with_conditioned_address(unmatched_address_table_2, "Address", "conditioned_address")
+
+        layer_creator.geocode_ffe_points_with_address_locator(unmatched_address_table_2, geo_locater_fc_path)
+
+
+
+        feature_classes_to_append.append(geo_locater_fc_path)
+
+    #combine all three geocoded_feature_classes
+if len(feature_classes_to_append) > 0:
+    layer_creator.append_tables_to_single_featureclass(ffe_main, feature_classes_to_append)
 
 ### Create the Map ##
 #pass the path only, the name is added in the function
 #create_map.create_survey_appender_from_template(output_mxd_path)
-
-### Create the layer from the input ffe excel spread sheet
-new_table = layer_creator.create_ffe_points_layer(input_excel, excel_sheet, output_gdb)
-
-
-feature_class_name = layer_creator.create_point_feature_class_with_template("FFE_points1")
-
-### Geocode the ffe points using a first pass from master address points and a second pass from the address locator
-unmatched_addresses = layer_creator.geocode_ffe_points_with_master_address_points(new_table, feature_class_name)
-
-
-### Geocode the ffe points using the taxlot addresses
-#unmatched_addresses = layer_creator.geocode_ffe_points_with_taxlots(new_table)
-
-###Go over the uncoded points using the adddress locator
-#layer_creator.geocode_ffe_points_with_address_locator(unmatched_addresses)
--- layer_creator.py
+++ layer_creator.py
@@ -1,32 +1,11 @@
 import arcpy
-
-# Assign variables to common paths
-
-
-
-#using output_gdb now, will transition to in memory
-output_gdb = r"C:\temp\ffe_scratch\FFE_working.gdb"
-
-if not arcpy.Exists(output_gdb):
-    arcpy.CreateFileGDB_management(r"C:\temp", "FFE_working.gdb")
-
-
-#input_excel = "//besfile1/ASM_Projects/E11166_Msspp/Research/Survey/Excel/Surveyed_E11166_07052019_AddOn.xls"
-
-#excel_sheet = "Table 1"
-
-#output_table = output_gdb + r"\new_ffe"
-
-#sr = arcpy.SpatialReference("NAD 1983 HARN StatePlane Oregon North FIPS 3601 (Intl Feet)")
-#ffe_template = output_gdb + "/ffe_template"
-
-
+import re
 
 egh_public = r"\\oberon\grp117\DAshney\Scripts\connections\egh_public on gisdb1.rose.portland.local.sde"
 
 #master_address = output_gdb + r"\master_address"
-master_address = egh_public + r"\ARCMAP_ADMIN.Master_Address_Points_pdx"
-address_locator = egh_public + r"\ARCMAP_ADMIN.Streets_Geocoding_pdx_no_zone"
+#master_address = egh_public + r"\ARCMAP_ADMIN.Master_Address_Points_pdx"
+
 
 ## Creates an empty feature class based on a template and converts the excel input to a table
 ## Returns the table path
@@ -36,25 +15,25 @@ def create_ffe_points_layer(input_excel, excel_sheet, output_gdb):
 
     arcpy.ExcelToTable_conversion(Input_Excel_File= input_excel, Output_Table=output_table, Sheet=excel_sheet)
 
+    arcpy.AddField_management(output_table, "Filtered_Address", "TEXT", "", "",50)
+
     return output_gdb + "/new_ffe"
 
 
-def create_point_feature_class_with_template(name):
+#need to be able to add the filtered addresses to the excel table
+
+def create_point_feature_class_with_template(name, output_gdb_path, template_path):
     sr = arcpy.SpatialReference("NAD 1983 HARN StatePlane Oregon North FIPS 3601 (Intl Feet)")
-    ffe_template = output_gdb + "/ffe_template"
-    arcpy.CreateFeatureclass_management(output_gdb, name, "POINT", ffe_template, "DISABLED", "DISABLED", sr)
-    return output_gdb + "/" + name
+    arcpy.CreateFeatureclass_management(output_gdb_path, name, "POINT", template_path, "DISABLED", "DISABLED", sr)
+    return output_gdb_path + "/" + name
 
 
 def append_tables_to_single_featureclass(main, additional):
     arcpy.Append_management(additional, main, "NO_TEST")
 
 ##Takes the table that does not have geometry, and creates a featureclass from it
-##
 def geocode_ffe_points_with_master_address_points(input_table, feature_class_path):
-
     input_table_fields = ["Address", "Elevation", "Basement"]
-    address_locator = egh_public + r"\ARCMAP_ADMIN.Streets_Geocoding_pdx_no_zone"
     master_address = egh_public + r"\ARCMAP_ADMIN.Master_Address_Points_pdx"
     insert_cursor = arcpy.da.InsertCursor(feature_class_path, input_table_fields)
     address_list =[]
@@ -74,131 +53,156 @@ def geocode_ffe_points_with_master_address_points(input_table, feature_class_pat
 
     #arcpy.Delete_management(input_table)
     # Create a layer that has the matching addresses and save it
-    arcpy.MakeFeatureLayer_management(master_address, "qt", query)
+    arcpy.MakeFeatureLayer_management(master_address, "temp_layer", query)
 
-    geometries = {key:value for (key,value) in arcpy.da.SearchCursor("qt", ["ADDRESS_FULL", 'Shape'])}
+    geometries = {key:value for (key,value) in arcpy.da.SearchCursor("temp_layer", ["ADDRESS_FULL", 'Shape'])}
 
     notfound = []
 
     #Update B with geometries from A where ID:s match
-    with arcpy.da.UpdateCursor(feature_class_path, ["Address", 'SHAPE@', 'Elevation', 'Basement']) as cursor:
+    with arcpy.da.UpdateCursor(feature_class_path, ["Address", 'SHAPE@', 'Elevation', 'Basement', 'GeocodingNotes']) as cursor:
         for row in cursor:
             try:
                 row[1] = geometries[row[0]]
+                row[4] = "Master Address"
                 cursor.updateRow(row)
             except:
                 notfound.append(tuple((row[0], row[2], row[3])))
+                cursor.deleteRow()
 
         del row, cursor
 
     print 'Found no id match and could not update geometry for IDs: ', notfound
+
     return notfound
 
 
-#####
-#####
-#####
 def geocode_ffe_points_with_taxlots(input_table, feature_class_path):
-
     input_table_fields = ["Address", "Elevation", "Basement"]
     taxlots = egh_public + r"\ARCMAP_ADMIN.taxlots_pdx"
     address_list = []
-
     insert_cursor = arcpy.da.InsertCursor(feature_class_path, input_table_fields)
-
     with arcpy.da.SearchCursor(input_table, input_table_fields) as search_cursor:
         for row in search_cursor:
             insert_cursor.insertRow(row)
             address_list.append(row[0])
-
         del search_cursor, insert_cursor
 
     address_list_2 = [str(r) for r in address_list]
     address_tuple = tuple(address_list_2)
-
     field_list = ["SITEADDR", "@SHAPE"]
     query = "SITECITY in('PORTLAND') AND SITEADDR in %s" % (address_tuple,)
-
     #arcpy.Delete_management(input_table)
-
-    # Create a layer that has the matching addresses and save it
-    arcpy.MakeFeatureLayer_management(taxlots, "temp_layer", query)
-
-    geometries = {key:value for (key,value) in arcpy.da.SearchCursor("temp_layer", ["SITEADDR", 'Shape'])}
-
+     # Create a layer that has the matching addresses and save it
+    arcpy.MakeFeatureLayer_management(taxlots, "temp_layer_2", query)
+    geometries = {key:value for (key,value) in arcpy.da.SearchCursor("temp_layer_2", ["SITEADDR", 'Shape'])}
     notfound = []
-
     #Update B with geometries from A where ID:s match
-    with arcpy.da.UpdateCursor(feature_class_path, ["Address", 'SHAPE@', 'Elevation', 'Basement']) as cursor:
+    with arcpy.da.UpdateCursor(feature_class_path, ["Address", 'SHAPE@', 'Elevation', 'Basement', 'GeocodingNotes']) as cursor:
         for row in cursor:
             try:
                 row[1] = geometries[row[0]]
+                row[4] = "Taxlot"
                 cursor.updateRow(row)
             except:
                 notfound.append(tuple((row[0], row[2], row[3])))
+                cursor.deleteRow()
 
         del row, cursor
 
     print 'Found no id match and could not update geometry for IDs: ', notfound
     return notfound
 
+def geocode_ffe_points_with_address_locator(not_found_path, feature_class_path):
+    address_locator = egh_public + r"\ARCMAP_ADMIN.Streets_Geocoding_pdx_no_zone"
+    arcpy.CalculateField_management(not_found_path, "City", "'Portland'", "PYTHON")
+    address_fields_city = "conditioned_address Address; City City"
+
+    arcpy.AlterField_management(not_found_path, "Address", "original_address")
+    arcpy.AlterField_management(not_found_path, "conditioned_address", "Address")
+
+    arcpy.GeocodeAddresses_geocoding(not_found_path, address_locator, "", feature_class_path)
+
+    arcpy.AlterField_management(feature_class_path, "Address", "conditioned_address")
+    arcpy.AlterField_management(feature_class_path, "original_address", "Address")
 
 
+    add_text_field_to_feature_class(feature_class_path,'GeocodingNotes', 15)
+
+    with arcpy.da.UpdateCursor(feature_class_path, ['GeocodingNotes', 'Status']) as cursor:
+        for row in cursor:
+            if row[1] =='U':
+                row[0] = "UNMATCHED"
+            else:
+                row[0] = "Address Locater"
+            cursor.updateRow(row)
 
 
+    drop_fields = ["Status", "Score", "Match_addr", "Pct_along", "Side", "Ref_ID", "X", "Y", "Addr_type", "ARC_Street", "ARC_City", "ARC_ZIP"]
+
+    if arcpy.TestSchemaLock(feature_class_path):
+        arcpy.DeleteField_management(feature_class_path, drop_fields)
+    else:
+        print("ArcPy will not unlock the geocoded ffe")
 
 
-def geocode_ffe_points_with_address_locator(not_found_list, feature_class_path):
-    # At this point there is a completed feature class that has spatial data for all points that match the master address list and we have a list of addresses that dont match
+def create_table_from_list(not_found_list, output_gdb_path, table_name):
+    full_table_path = output_gdb_path + "/" + table_name
+    arcpy.CreateTable_management(output_gdb_path, table_name)
+    arcpy.AddField_management(full_table_path, 'Address', "TEXT", "", "", 50)
+    arcpy.AddField_management(full_table_path, 'Elevation', "FLOAT", )
+    arcpy.AddField_management(full_table_path, 'Basement', "TEXT", "", "", 5)
+    arcpy.AddField_management(full_table_path, 'City', "TEXT", "", "", 8)
+    cursor = arcpy.da.InsertCursor(full_table_path, ['Address', 'Elevation', 'Basement'])
+    for row in not_found_list:
+        cursor.insertRow(row)
+    del row, cursor
+    return full_table_path
 
-### create a new table from the unmatched records using the other table as a template
- #TODO: these use file paths that sould be removed
+def remove_address_ranges_from_list_of_addresses(list):
+    new_list = []
+    regex_1 = re.compile(r'(?:-\w+)+') # this gets the hyphen without spaces
+    regex_2 = re.compile(r'(?:-+\s\w+)+') # this gets the hyphen that includes spaces
 
-    arcpy.CreateTable_management(r"C:\temp\ffe_scratch\ffe_working.gdb", "unmatched_ffe" )
-
-    arcpy.AddField_management(r"C:\temp\ffe_scratch\FFE_working.gdb\unmatched_ffe", 'Address', "TEXT", "", "", 50)
-    arcpy.AddField_management(r"C:\temp\ffe_scratch\FFE_working.gdb\unmatched_ffe", 'Elevation', "FLOAT",)
-    arcpy.AddField_management(r"C:\temp\ffe_scratch\FFE_working.gdb\unmatched_ffe", 'Basement', "TEXT", "", "", 5)
-
-    arcpy.AddField_management(r"C:\temp\ffe_scratch\FFE_working.gdb\unmatched_ffe", 'City', "TEXT", "", "", 8)
-
-
-    if not_found_list:
-        cursor = arcpy.da.InsertCursor(r"C:\temp\ffe_scratch\FFE_working.gdb\unmatched_ffe", ['Address', 'Elevation', 'Basement'])
-
-        for row in not_found_list:
-            cursor.insertRow(row)
-
-        del row, cursor
-
-        arcpy.CalculateField_management(r"C:\temp\ffe_scratch\FFE_working.gdb\unmatched_ffe", "City", "'Portland'", "PYTHON")
-
-        # run that table through the geocode process to create geometry for the remaining points
-
-        arcpy.GeocodeAddresses_geocoding(r"C:\temp\ffe_scratch\ffe_working.gdb\unmatched_ffe", address_locator, "Address Address", r"C:\temp\ffe_scratch\ffe_working.gdb\geocoded_ffe")
+    for i in list:
+        if re.search(r'(?:-\w+)+', i[0]):
+            replacement_address = regex_1.sub("", i[0])
+            new_tuple = (replacement_address, i[1], i[2])
+            new_list.append(new_tuple)
+        elif re.search(r'(?:-+\s\w+)+', i[0]):
+            replacement_address = regex_2.sub("", i[0])
+            new_tuple = (replacement_address, i[1], i[2])
+            new_list.append(new_tuple)
+        else:
+            new_tuple = (i[0], i[1], i[2])
+            new_list.append(new_tuple)
 
 
+    return new_list
 
-        #create a lookup for the geometries based on their address
+def filter_address_with_regex(address):
+    regex_1 = re.compile(r'(?:-\w+)+') # this gets the hyphen without spaces
+    regex_2 = re.compile(r'(?:-+\s\w+)+') # this gets the hyphen that includes spaces
+    regex_3 = re.compile(r'(?:\s(\d+)\b)') #this gets the case where we have 2 numbers with no hyphen
 
-        arcpy.MakeFeatureLayer_management(r"C:\temp\ffe_scratch\ffe_working.gdb\geocoded_ffe", "qc")
+    if re.search(r'(?:-\w+)+', address):
+        replacement_address = regex_1.sub("", address)
+        return replacement_address
+    elif re.search(r'(?:-+\s\w+)+', address):
+        replacement_address = regex_2.sub("", address)
+        return replacement_address
+    elif re.search(r'(?:\s(\d+)\b)', address):
+        replacement_address = regex_3.sub("", address)
+        return replacement_address
+    else:
+        return address
 
-        geometries2 = {key:value for (key,value) in arcpy.da.SearchCursor("qc", ["ARC_Single_Line_Input", 'Shape'])}
+def add_text_field_to_feature_class(feature_class, field_name, text_length):
 
-        notfound2 = []
+    arcpy.AddField_management(feature_class, field_name,"TEXT", "", "", text_length)
 
-        #Update B with geometries from A where ID:s match
-        with arcpy.da.UpdateCursor(feature_class_path, ["Address", 'SHAPE@', 'Elevation', 'Basement']) as cursor:
-            for row in cursor:
-                try:
-                    row[1] = geometries2[row[0]]
-                    cursor.updateRow(row)
-                except:
-                    notfound2.append(tuple((row[0], row[2], row[3])))
-
-            del row, cursor
-
-        print 'Found no id match and could not update geometry for IDs: ', notfound2
-
-        # Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
-        # The following inputs are layers or table views: "unmatched_ffe"
+def update_field_with_conditioned_address(feature_class, source_field_name, update_field_name):
+    with arcpy.da.UpdateCursor(feature_class, [source_field_name, update_field_name]) as cursor:
+        for row in cursor:
+            row[1] = filter_address_with_regex(row[0])
+            cursor.updateRow(row)
